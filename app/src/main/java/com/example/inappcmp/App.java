12f45860a8d1d7f7cd0701a8e14fdd57d4f66d98
Reyst <oleksenko.pm@gmail.com>
2020-12-07 12:11:20 +0200

    check change listening

--- app/src/main/java/com/example/inappcmp/App.java
+++ app/src/main/java/com/example/inappcmp/App.java
@@ -5,13 +5,35 @@ import android.util.Log;
 
 import com.opencmp.inapplib.OpenCmp;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.opencmp.inapplib.OpenCmpStore;
+
 public class App extends Application {
+    SharedPreferences.OnSharedPreferenceChangeListener prefListener;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         OpenCmp.initialize(this, "traffective.com", this::handleError);
+
+        // Check Consent Changes
+        prefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Log.i("OpenCmp", "Preferences changed:" + key);
+                if (key.equals(OpenCmpStore.Property.IABTCF_TCString.name())) {
+                    App.this.consentChanged();
+                }
+            }
+        };
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(prefListener);
+    }
+
+    private void consentChanged() {
+        Log.i("OpenCmp", "Consent changed");
     }
 
     private void handleError(Exception e) {
